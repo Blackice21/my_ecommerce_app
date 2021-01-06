8c69bd59f500ff430224d91a42e5878e08e2e010
Marcus <marcusmorrow319@gmail.com>
2021-01-06 12:13:50 -0600

    now have chapter and exercise detail views.

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.book_list, name='book_list'),
     path('<slug>/detail', views.book_detail, name='book_detail'),
+    path('<book_slug>/<chapter_number>', views.chapter_detail, name='chapter_detail'),
+    path('<book_slug>/<chapter_number>/<chapter_title>/<ex_number>', views.exercise_detail, name='exercise_detail'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Book
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book, Chapter, Exercise
+from django.http import Http404
 # Create your views here.
 def book_list(request):
     book_list = Book.objects.all()
@@ -15,4 +16,25 @@ def book_detail(request, slug):
     }
     return render(request, 'book_detail.html', context)
 
+def chapter_detail(request, book_slug, chapter_number):
+    chapters = Chapter.objects \
+              .filter(book__slug=book_slug) \
+              .filter(chapter_number=chapter_number)
+
+    if chapters.exists():
+        context={'chapter': chapters[0]}
+        return render(request, 'chapter_detail.html', context)
+    raise Http404
+
+def exercise_detail(request, book_slug, chapter_number, chapter_title, ex_number):
+    ex_qs = Exercise.objects \
+              .filter(chapter__book__slug=book_slug) \
+              .filter(chapter__chapter_number=chapter_number) \
+              .filter(chapter__title=chapter_title) \
+              .filter(exercise_number=ex_number)
+
+    if ex_qs.exists():
+        context={'exercise': ex_qs[0]}
+        return render(request, 'exercise_detail.html', context)
+    raise Http404
 
