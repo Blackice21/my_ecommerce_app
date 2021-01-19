b53564d8f1742a7c65c62670fe9d4be5da5825a9
Marcus <marcusmorrow319@gmail.com>
2021-01-19 11:46:39 -0600

    set up models for shopping cart models

--- django_ecommerce_pro/settings.py
+++ django_ecommerce_pro/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'books',
+    'shopping_cart',
     # allauth apps
     'django.contrib.sites',
     'allauth',
---
+++ shopping_cart/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.conf import settings
+from books.models import Book
+
+# Create your models here.
+class OrderItem(models.Model):
+    books = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.book.title
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    ref_code = models.CharField(max_length=200)
+    is_ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.ref_code
+
+class Payment(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    total = models.FloatField()
+    stripe_charge_id = models.CharField(max_length=100)
+    date_paid = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.stripe_charge_id
